eb576ffc64165cb07e9b3cf77696aa6327518636
Jeff Blue <517262600@qq.com>
2020-07-10 11:08:22 +0800

    add headers
    1

---
+++ headers/call_header.js
@@ -0,0 +1,116 @@
+// **********************
+// just need to :
+// 1. Correct meta.content
+// 2. $_ts pass in
+// 3. Correct window instanceof  Window use "!_\$.{2}&&_\$.{2}&&_\$.{2};" replace to "false;"
+// 4. Add this header to front of the code
+// **********************
+//
+
+// $_ts define here !
+$_ts = '';
+// meta.content here !
+meta = {};
+meta.content = '{qqqqqqhPduF5x40dx_qksWGqqqq.Pem2FrP_2CfPq9hILc98xwStDw4sSX0ecMHVg2dNGSql4096~FV7mbXcsnt.Q8.CbQxLg.PpXbFBEPGcFl3LLSBbbDi_93jOCnF59yXAPMH_mOgDb3cxYbBf5Ac5qQzcI9EiT2XDbOFN9cfu8sWJZVGnKkDgqqGKbcrEQT0ajOrwRbNSbElZx29DbqD_Av7fC_qgJPar1EiLLIasDFrQLd7SOtHRlT81wWxKJL1SgaiTqpVbJOm0QeWOpJDkZysPrHlU0N3KfsoU9hxSlbrba4QPQzH0WIUsmsJ0zG3KwzIU9T8CENRrYe1nqRWrqnY1qQJl0vA1lQIV0TsCEwRr2epnqYoDWmwSfFmaqvpsyOFmrbkurgR2qc1ppPRaqqKDNHoK0Q1AW.i1RLqqqqqVlRqGN1PPEtlfNPiOEtALt1074790440XAOo30eljTuAgmeehhIUrtA;4kUyzUi8kgD7ll6J2MqFBA;lEU9iqqq!x7z,aac,amr,asm,avi,bak,bat,bmp,bin,c,cab,css,csv,com,cpp,dat,dll,doc,dot,docx,exe,eot,fla,flc,fon,fot,font,gdb,gif,gz,gho,hlp,hpp,htc,ico,ini,inf,ins,iso,js,jar,jpg,jpeg,json,java,lib,log,mid,mp4,mpa,m4a,mp3,mpg,mkv,mod,mov,mim,mpp,msi,mpeg,obj,ocx,ogg,olb,ole,otf,py,pyc,pas,pgm,ppm,pps,ppt,pdf,pptx,png,pic,pli,psd,qif,qtx,ra,rm,ram,rmvb,reg,res,rtf,rar,so,sbl,sfx,swa,swf,svg,sys,tar,taz,tif,tiff,torrent,txt,ttf,vsd,vss,vsw,vxd,woff,woff2,wmv,wma,wav,wps,xbm,xpm,xls,xlsx,xsl,xml,z,zip,apk,plist,ipar1k162MpUwapUpJQ92esoJhQ0ya1oxJwV0HpFf6boDIwRen96Aqqqql3650r0Ddfe167loxVuqqqqr0hw3WIqJIArCdc80KPZCkcucV0VwfSw4zph9sGqqqqqr0YeeDKRz_eQZKzkI8v9YLOsk118|gtN78aCc7pFyg.Kka1imvS9srKX2Hgf613XJR0bs4Qiz8Cb5MK8w3TCc388gRgYO91Ie_6SdspdzGf6PmoIYBgChq8wwySYjipeSDno5WUEQMZUPaD.r.zmtZAjmFvl5boHJydbDvDRS3zCcdsXmoXobdlL2P7SibKhNEg0Fo3.w2OlK3rhqM2TuL1iyqfYis8jNkTlDZIgSk295TRJyJ6TOwIgJcBfPM8Efh2TnGoxwmXC1S3jpFj2_IQLE1aUnaR_SMBV_F1ygs9DK5MCyxWDqS86r9MUE6RlfBqqqqqq}Jpbf0VvxwIqEBDkEkJrpLoOWQhPWnlkEUx9mZAoJHJpQGmOApFq9uc1gTtAGBDnZwxa7B4nOfwhfd7vn7tiqSNGHTJiQ0LK59tHg.4PHdRje47bM2tHVOecdeEdrb_sjCtHESXqH.JHV07C5nJ89vd1CCRLRGGshAWZ3q';
+
+// headers start --------------------------------->
+nativecode = '';
+function DOMParser(){[nativecode]}
+var Window = function(){};
+let window = new Window();
+window.$_ts = $_ts;
+navigator = {};
+document = {};
+window.indexedDB = {};
+window.top = window;
+window.document = {};
+sessionStorage = {};
+window.setTimeout = setTimeout;
+window.setInterval = setInterval;
+window.eval = eval;
+window.escape = escape;
+window.Number = Number;
+window.isFinite = isFinite;
+window.location = {"href":"http://wenshu.court.gov.cn/","ancestorOrigins":{},"origin":"http://wenshu.court.gov.cn","protocol":"http:","host":"wenshu.court.gov.cn","hostname":"wenshu.court.gov.cn","port":"","pathname":"/","search":"","hash":""};
+window.JSON = JSON;
+window.DOMParser = {};//DOMParser;
+window.RegExp = RegExp;
+window.location = {"href":"http://wenshu.court.gov.cn/website/wenshu/181217BMTKHNT2W0/index.html?pageId=b0c1f68c59d12f497984cda46ad7d8f1","ancestorOrigins":{},"origin":"http://wenshu.court.gov.cn","protocol":"http:","host":"wenshu.court.gov.cn","hostname":"wenshu.court.gov.cn","port":"","pathname":"/website/wenshu/181217BMTKHNT2W0/index.html","search":"?pageId=b0c1f68c59d12f497984cda46ad7d8f1","hash":""};
+window.addEventListener = function(target, recall, b){console.log('target = ',target, 'recall = ',recall)};
+localStorage = window.localStorage = {};
+window.localStorage.removeItem = function(a=''){console.log('localStorage.removeItem', a);this[a] = undefined;};
+window.localStorage.setItem = function(name='', value=''){window.localStorage[name] = value;}
+window.sessionStorage = sessionStorage;
+window.webkitRequestFileSystem = function(){[nativecode]};
+window.clearInterval = function(){};
+window.Math = Math;
+window.name = '';
+window.navigator = navigator;
+window.navigator.mimeTypes = {};
+window.document = document;
+window.self=window.top;
+window.openDatabase = function(a='',b='',c='',d=''){console.log('openDatabase: (', a, b, c, d, ')');return {}};
+// window instanceof Window can't be stimulated..... has to change the code at #1
+// use "!_\$.{2}&&_\$.{2}&&_\$.{2};" to regex and replace with "false;"
+// check compressed functions at #2 (its ok if dont format the codes )
+// localStorage pass variables but i can't stimulate it ...  (I can !!!!) #3
+class Request{
+    constructor(){
+        this.open = function(a='',b='',c=''){
+            console.log('XMLrequest.open ',a, b, c)
+        };
+        this.send = function (a='',b='') {
+            console.log('XMLrequest.send ',a, b)
+        }
+    }
+}
+window.XMLHttpRequest = Request;
+window.chrome = {"app":{"isInstalled":false,"InstallState":{"DISABLED":"disabled","INSTALLED":"installed","NOT_INSTALLED":"not_installed"},"RunningState":{"CANNOT_RUN":"cannot_run","READY_TO_RUN":"ready_to_run","RUNNING":"running"}}};
+navigator.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36";
+navigator.platform = "Win32";
+navigator.language = "zh-CN";
+navigator.appVersion = "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
+navigator.getBattery = function(){};
+navigator.webkitPersistentStorage = {};
+document.exitFullscreen = function(){console.log('call exitFullscreen')};
+document.addEventListener = window.addEventListener;
+document.documentElement = {};
+document.documentElement.style = {"alignContent":"","alignItems":"","alignSelf":"","alignmentBaseline":"","all":"","animation":"","animationDelay":"","animationDirection":"","animationDuration":"","animationFillMode":"","animationIterationCount":"","animationName":"","animationPlayState":"","animationTimingFunction":"","backdropFilter":"","backfaceVisibility":"","background":"","backgroundAttachment":"","backgroundBlendMode":"","backgroundClip":"","backgroundColor":"","backgroundImage":"","backgroundOrigin":"","backgroundPosition":"","backgroundPositionX":"","backgroundPositionY":"","backgroundRepeat":"","backgroundRepeatX":"","backgroundRepeatY":"","backgroundSize":"","baselineShift":"","blockSize":"","border":"","borderBlockEnd":"","borderBlockEndColor":"","borderBlockEndStyle":"","borderBlockEndWidth":"","borderBlockStart":"","borderBlockStartColor":"","borderBlockStartStyle":"","borderBlockStartWidth":"","borderBottom":"","borderBottomColor":"","borderBottomLeftRadius":"","borderBottomRightRadius":"","borderBottomStyle":"","borderBottomWidth":"","borderCollapse":"","borderColor":"","borderImage":"","borderImageOutset":"","borderImageRepeat":"","borderImageSlice":"","borderImageSource":"","borderImageWidth":"","borderInlineEnd":"","borderInlineEndColor":"","borderInlineEndStyle":"","borderInlineEndWidth":"","borderInlineStart":"","borderInlineStartColor":"","borderInlineStartStyle":"","borderInlineStartWidth":"","borderLeft":"","borderLeftColor":"","borderLeftStyle":"","borderLeftWidth":"","borderRadius":"","borderRight":"","borderRightColor":"","borderRightStyle":"","borderRightWidth":"","borderSpacing":"","borderStyle":"","borderTop":"","borderTopColor":"","borderTopLeftRadius":"","borderTopRightRadius":"","borderTopStyle":"","borderTopWidth":"","borderWidth":"","bottom":"","boxShadow":"","boxSizing":"","breakAfter":"","breakBefore":"","breakInside":"","bufferedRendering":"","captionSide":"","caretColor":"","clear":"","clip":"","clipPath":"","clipRule":"","color":"","colorInterpolation":"","colorInterpolationFilters":"","colorRendering":"","colorScheme":"","columnCount":"","columnFill":"","columnGap":"","columnRule":"","columnRuleColor":"","columnRuleStyle":"","columnRuleWidth":"","columnSpan":"","columnWidth":"","columns":"","contain":"","containIntrinsicSize":"","content":"","counterIncrement":"","counterReset":"","cursor":"","cx":"","cy":"","d":"","direction":"","display":"","dominantBaseline":"","emptyCells":"","fill":"","fillOpacity":"","fillRule":"","filter":"","flex":"","flexBasis":"","flexDirection":"","flexFlow":"","flexGrow":"","flexShrink":"","flexWrap":"","float":"","floodColor":"","floodOpacity":"","font":"","fontDisplay":"","fontFamily":"","fontFeatureSettings":"","fontKerning":"","fontOpticalSizing":"","fontSize":"","fontStretch":"","fontStyle":"","fontVariant":"","fontVariantCaps":"","fontVariantEastAsian":"","fontVariantLigatures":"","fontVariantNumeric":"","fontVariationSettings":"","fontWeight":"","gap":"","grid":"","gridArea":"","gridAutoColumns":"","gridAutoFlow":"","gridAutoRows":"","gridColumn":"","gridColumnEnd":"","gridColumnGap":"","gridColumnStart":"","gridGap":"","gridRow":"","gridRowEnd":"","gridRowGap":"","gridRowStart":"","gridTemplate":"","gridTemplateAreas":"","gridTemplateColumns":"","gridTemplateRows":"","height":"","hyphens":"","imageOrientation":"","imageRendering":"","inlineSize":"","isolation":"","justifyContent":"","justifyItems":"","justifySelf":"","left":"","letterSpacing":"","lightingColor":"","lineBreak":"","lineHeight":"","listStyle":"","listStyleImage":"","listStylePosition":"","listStyleType":"","margin":"","marginBlockEnd":"","marginBlockStart":"","marginBottom":"","marginInlineEnd":"","marginInlineStart":"","marginLeft":"","marginRight":"","marginTop":"","marker":"","markerEnd":"","markerMid":"","markerStart":"","mask":"","maskType":"","maxBlockSize":"","maxHeight":"","maxInlineSize":"","maxWidth":"","maxZoom":"","minBlockSize":"","minHeight":"","minInlineSize":"","minWidth":"","minZoom":"","mixBlendMode":"","objectFit":"","objectPosition":"","offset":"","offsetDistance":"","offsetPath":"","offsetRotate":"","opacity":"","order":"","orientation":"","orphans":"","outline":"","outlineColor":"","outlineOffset":"","outlineStyle":"","outlineWidth":"","overflow":"","overflowAnchor":"","overflowWrap":"","overflowX":"","overflowY":"","overscrollBehavior":"","overscrollBehaviorBlock":"","overscrollBehaviorInline":"","overscrollBehaviorX":"","overscrollBehaviorY":"","padding":"","paddingBlockEnd":"","paddingBlockStart":"","paddingBottom":"","paddingInlineEnd":"","paddingInlineStart":"","paddingLeft":"","paddingRight":"","paddingTop":"","pageBreakAfter":"","pageBreakBefore":"","pageBreakInside":"","paintOrder":"","perspective":"","perspectiveOrigin":"","placeContent":"","placeItems":"","placeSelf":"","pointerEvents":"","position":"","quotes":"","r":"","resize":"","right":"","rowGap":"","rx":"","ry":"","scrollBehavior":"","scrollMargin":"","scrollMarginBlock":"","scrollMarginBlockEnd":"","scrollMarginBlockStart":"","scrollMarginBottom":"","scrollMarginInline":"","scrollMarginInlineEnd":"","scrollMarginInlineStart":"","scrollMarginLeft":"","scrollMarginRight":"","scrollMarginTop":"","scrollPadding":"","scrollPaddingBlock":"","scrollPaddingBlockEnd":"","scrollPaddingBlockStart":"","scrollPaddingBottom":"","scrollPaddingInline":"","scrollPaddingInlineEnd":"","scrollPaddingInlineStart":"","scrollPaddingLeft":"","scrollPaddingRight":"","scrollPaddingTop":"","scrollSnapAlign":"","scrollSnapStop":"","scrollSnapType":"","shapeImageThreshold":"","shapeMargin":"","shapeOutside":"","shapeRendering":"","size":"","speak":"","src":"","stopColor":"","stopOpacity":"","stroke":"","strokeDasharray":"","strokeDashoffset":"","strokeLinecap":"","strokeLinejoin":"","strokeMiterlimit":"","strokeOpacity":"","strokeWidth":"","tabSize":"","tableLayout":"","textAlign":"","textAlignLast":"","textAnchor":"","textCombineUpright":"","textDecoration":"","textDecorationColor":"","textDecorationLine":"","textDecorationSkipInk":"","textDecorationStyle":"","textIndent":"","textOrientation":"","textOverflow":"","textRendering":"","textShadow":"","textSizeAdjust":"","textTransform":"","textUnderlinePosition":"","top":"","touchAction":"","transform":"","transformBox":"","transformOrigin":"","transformStyle":"","transition":"","transitionDelay":"","transitionDuration":"","transitionProperty":"","transitionTimingFunction":"","unicodeBidi":"","unicodeRange":"","userSelect":"","userZoom":"","vectorEffect":"","verticalAlign":"","visibility":"","webkitAlignContent":"","webkitAlignItems":"","webkitAlignSelf":"","webkitAnimation":"","webkitAnimationDelay":"","webkitAnimationDirection":"","webkitAnimationDuration":"","webkitAnimationFillMode":"","webkitAnimationIterationCount":"","webkitAnimationName":"","webkitAnimationPlayState":"","webkitAnimationTimingFunction":"","webkitAppRegion":"","webkitAppearance":"","webkitBackfaceVisibility":"","webkitBackgroundClip":"","webkitBackgroundOrigin":"","webkitBackgroundSize":"","webkitBorderAfter":"","webkitBorderAfterColor":"","webkitBorderAfterStyle":"","webkitBorderAfterWidth":"","webkitBorderBefore":"","webkitBorderBeforeColor":"","webkitBorderBeforeStyle":"","webkitBorderBeforeWidth":"","webkitBorderBottomLeftRadius":"","webkitBorderBottomRightRadius":"","webkitBorderEnd":"","webkitBorderEndColor":"","webkitBorderEndStyle":"","webkitBorderEndWidth":"","webkitBorderHorizontalSpacing":"","webkitBorderImage":"","webkitBorderRadius":"","webkitBorderStart":"","webkitBorderStartColor":"","webkitBorderStartStyle":"","webkitBorderStartWidth":"","webkitBorderTopLeftRadius":"","webkitBorderTopRightRadius":"","webkitBorderVerticalSpacing":"","webkitBoxAlign":"","webkitBoxDecorationBreak":"","webkitBoxDirection":"","webkitBoxFlex":"","webkitBoxOrdinalGroup":"","webkitBoxOrient":"","webkitBoxPack":"","webkitBoxReflect":"","webkitBoxShadow":"","webkitBoxSizing":"","webkitClipPath":"","webkitColumnBreakAfter":"","webkitColumnBreakBefore":"","webkitColumnBreakInside":"","webkitColumnCount":"","webkitColumnGap":"","webkitColumnRule":"","webkitColumnRuleColor":"","webkitColumnRuleStyle":"","webkitColumnRuleWidth":"","webkitColumnSpan":"","webkitColumnWidth":"","webkitColumns":"","webkitFilter":"","webkitFlex":"","webkitFlexBasis":"","webkitFlexDirection":"","webkitFlexFlow":"","webkitFlexGrow":"","webkitFlexShrink":"","webkitFlexWrap":"","webkitFontFeatureSettings":"","webkitFontSizeDelta":"","webkitFontSmoothing":"","webkitHighlight":"","webkitHyphenateCharacter":"","webkitJustifyContent":"","webkitLineBreak":"","webkitLineClamp":"","webkitLocale":"","webkitLogicalHeight":"","webkitLogicalWidth":"","webkitMarginAfter":"","webkitMarginBefore":"","webkitMarginEnd":"","webkitMarginStart":"","webkitMask":"","webkitMaskBoxImage":"","webkitMaskBoxImageOutset":"","webkitMaskBoxImageRepeat":"","webkitMaskBoxImageSlice":"","webkitMaskBoxImageSource":"","webkitMaskBoxImageWidth":"","webkitMaskClip":"","webkitMaskComposite":"","webkitMaskImage":"","webkitMaskOrigin":"","webkitMaskPosition":"","webkitMaskPositionX":"","webkitMaskPositionY":"","webkitMaskRepeat":"","webkitMaskRepeatX":"","webkitMaskRepeatY":"","webkitMaskSize":"","webkitMaxLogicalHeight":"","webkitMaxLogicalWidth":"","webkitMinLogicalHeight":"","webkitMinLogicalWidth":"","webkitOpacity":"","webkitOrder":"","webkitPaddingAfter":"","webkitPaddingBefore":"","webkitPaddingEnd":"","webkitPaddingStart":"","webkitPerspective":"","webkitPerspectiveOrigin":"","webkitPerspectiveOriginX":"","webkitPerspectiveOriginY":"","webkitPrintColorAdjust":"","webkitRtlOrdering":"","webkitRubyPosition":"","webkitShapeImageThreshold":"","webkitShapeMargin":"","webkitShapeOutside":"","webkitTapHighlightColor":"","webkitTextCombine":"","webkitTextDecorationsInEffect":"","webkitTextEmphasis":"","webkitTextEmphasisColor":"","webkitTextEmphasisPosition":"","webkitTextEmphasisStyle":"","webkitTextFillColor":"","webkitTextOrientation":"","webkitTextSecurity":"","webkitTextSizeAdjust":"","webkitTextStroke":"","webkitTextStrokeColor":"","webkitTextStrokeWidth":"","webkitTransform":"","webkitTransformOrigin":"","webkitTransformOriginX":"","webkitTransformOriginY":"","webkitTransformOriginZ":"","webkitTransformStyle":"","webkitTransition":"","webkitTransitionDelay":"","webkitTransitionDuration":"","webkitTransitionProperty":"","webkitTransitionTimingFunction":"","webkitUserDrag":"","webkitUserModify":"","webkitUserSelect":"","webkitWritingMode":"","whiteSpace":"","widows":"","width":"","willChange":"","wordBreak":"","wordSpacing":"","wordWrap":"","writingMode":"","x":"","y":"","zIndex":"","zoom":""};
+document.characterSet = 'UTF-8';
+document.charset = 'UTF-8';
+script1 = {};
+script1.src = "http://wenshu.court.gov.cn/gGK4jBsBBszn/c3aqs9gktfr4.dfe1675.js";
+script2 = {};
+script2.r = 'm';
+document.scripts=[script1, script2];
+meta.parentNode = {};
+meta.parentNode.removeChild = function(a=''){console.log('removeChild', a);};
+document.getElementsByTagName = function(a){
+    console.log('get tag', a);
+    if (a === 'meta'){
+        return [meta]
+    }
+    else{
+        return []
+    }
+};
+document.cookie='';
+divtag = {};
+divtag.getElementsByTagName = function(){let res = {};res.getElementsByTagName=function(){return []}; return res;};
+divtag.parentNode = {};
+divtag.parentNode.removeChild = function(a='',b=''){};
+document.createElement = function(name){
+    return divtag;
+};
+// Hook Date
+Date.prototype._getTime = Date.prototype.getTime;
+Date.prototype.getTime = function(){
+    if(!Date._lastTime){
+        var time = 1593132851103;
+        Date._lastTime = time;
+    }
+    Date._lastTime = Date._lastTime+100;
+    return Date._lastTime;
+};
+
+//  headers end ----------------------------- >
